c3240103fbd29417bf40da8b114055ba87b7184e
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-05-26 17:21:53 +0900

    [Passed] 특정 구간의 합 (2ms, 0MB)

---
+++ trail2/특정 구간의 합/sum-of-specific-intervals.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[100];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a1, a2, sum = 0;
+        cin >> a1 >> a2;
+        for (int j = a1; j <= a2; ++j) {
+            sum += arr[j - 1];
+        }
+        cout << sum << "\n";
+    }
+
+    // Please write your code here.
+
+
+    return 0;
+}
