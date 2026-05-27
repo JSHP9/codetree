9b0e968127f90264b86a734ca849d9b027b009ef
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-05-27 16:48:44 +0900

    [Passed] 100으로 나눈 나머지의 수열 (2ms, 0MB)

---
+++ trail2/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Sequence(int n) {
+    if (n == 1) 
+        return 2;
+    if (n == 2)
+        return 4;
+    return (Sequence(n - 1) * Sequence(n - 2)) % 100;
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << Sequence(n);
+    return 0;
+}
