dd04c9b66939288746474d913a1638581a902553
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-06-01 23:50:07 +0900

    [Passed] Time to Time (2ms, 0MB)

---
+++ trail2/Time to Time/time-to-time.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int start = a * 60 + b;
+    int end = c * 60 + d;
+
+    cout << end - start;
+    
+    return 0;
+}
