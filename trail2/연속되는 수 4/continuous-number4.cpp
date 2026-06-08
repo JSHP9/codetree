b406f70703cfb4c5785ff32214185f688a4801c7
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-06-08 15:38:22 +0900

    [Passed] 연속되는 수 4 (2ms, 0MB)

---
+++ trail2/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    int max = 0, cnt = 1;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+        if (i == 0 || arr[i] <= arr[i - 1]) {
+            cnt = 1;
+        }
+        else {
+            cnt++;
+        }
+        if (max < cnt)
+            max = cnt;
+    }
+    cout << max;
+    return 0;
+}
