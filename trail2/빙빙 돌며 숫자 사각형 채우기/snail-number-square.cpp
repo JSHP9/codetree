5ed81c81270be9a774efdc1826fce77176bee3ed
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-06-10 12:29:40 +0900

    [Passed] 빙빙 돌며 숫자 사각형 채우기 (2ms, 0MB)

---
+++ trail2/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+bool IsRange(int x, int y, int n, int m) {
+    return (1 <= x && x <= n && 1 <= y && y <= m);
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int arr[n + 1][m  + 1] = {};
+    int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+    int x = 1, y = 1;
+    int nx, ny;
+    int dir = 0;
+    arr[1][1] = 1;
+    for (int i = 2; i <= n * m; ++i) {
+        nx = x + dx[dir], ny = y + dy[dir];
+        if (!IsRange(nx, ny, n, m) || arr[nx][ny] != 0) {
+            dir = (dir + 1) % 4;
+        }
+        x += dx[dir], y += dy[dir];
+        arr[x][y] = i;
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
