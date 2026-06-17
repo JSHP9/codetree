f2791d2d4a5f58462a48e53bc2ca1dda4e1c9cd0
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-06-17 15:20:09 +0900

    [Passed] 한 가지로 열리는 자물쇠 (2ms, 0MB)

---
+++ trail2/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[3];
+    for (int i = 0; i < 3; ++i)
+        cin >> arr[i];
+
+    int cnt = 0;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            for (int k = 1; k <= n; ++k) {
+                if (abs(arr[0] - i) <= 2 || abs(arr[1] - j) <= 2 || abs(arr[2] - k) <= 2)
+                    cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
