2883b7523383ab5695a12d8c0a389532868c607e
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-06-17 18:11:08 +0900

    [Passed] 개발자의 능력 2 (2ms, 0MB)

---
+++ trail2/개발자의 능력 2/ability-of-developer-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int ability[6];
+
+int dist(int a, int b, int c) {
+    int m = (a > b) ? a : b;
+    int s = a + b - m;
+    m = (c > m) ? c : m;
+    s = (c > s) ? s : c;
+    return m - s;
+}
+
+int main() {
+    int sum = 0;
+    for (int i = 0; i < 6; i++) {
+        cin >> ability[i];
+        sum += ability[i];
+    }
+
+    int sumCpy = sum;
+    int ans = INT_MAX;
+    int t1, t2 ,t3;
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            for (int k = 0; k < 6; ++k) {
+                for (int l = 0; l < 6; ++l) { 
+                    sum = sumCpy;
+                    if (i != j && i != k && i != l && j != k && j != l && k != l) {
+                        t1 = ability[i] + ability[j];
+                        t2 = ability[k] + ability[l];
+                        t3 = sum - t1 -t2;
+                        ans = min(ans, dist(t1, t2, t3));
+                    }
+                }
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
