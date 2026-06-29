c7bd0382eb6f1db5f90dab8fc83be0e2d68d5610
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-06-29 15:50:39 +0900

    [Passed] 세 수의 최대 곱 (2ms, 0MB)

---
+++ trail2/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0 ; i < n; ++i)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    long long a = arr[n-1] * arr[n-2] * arr[n-3];
+    long long b = arr[0] * arr[1] * arr[n-1];
+    cout << max(a, b);
+    return 0;
+}
