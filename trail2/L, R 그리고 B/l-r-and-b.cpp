f7ccabb3031a5c72dbada693e9962a59871aeec4
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-06-29 15:21:52 +0900

    [Passed] L, R 그리고 B (2ms, 0MB)

---
+++ trail2/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+char board[10][10];
+bool visited[10][10];
+int dist[10][10];
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+int main() {
+    queue<pair<int,int>> q;
+
+    int sx, sy;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cin >> board[i][j];
+
+            if (board[i][j] == 'L') {
+                sx = i;
+                sy = j;
+            }
+        }
+    }
+
+    q.push({sx, sy});
+    visited[sx][sy] = true;
+
+    while (!q.empty()) {
+
+        pair<int,int> cur = q.front();
+        q.pop();
+
+        int x = cur.first;
+        int y = cur.second;
+
+        for (int i = 0; i < 4; i++) {
+
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= 10 || ny < 0 || ny >= 10)
+                continue;
+            if (visited[nx][ny] == true)
+                continue;
+            if (board[nx][ny] == 'R')
+                continue;
+            // ④ 방문 처리
+            visited[nx][ny] = true;
+            dist[nx][ny] = dist[x][y] + 1;
+
+            if (board[nx][ny] =='B') {
+                cout << dist[nx][ny] - 1;
+                return 0;
+            }
+
+            q.push({nx, ny});
+        }
+    }
+}
