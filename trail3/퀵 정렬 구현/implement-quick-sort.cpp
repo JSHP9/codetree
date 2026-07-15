f58b7a68bc2a938e2d12711958e97d8998ab8c9f
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-07-15 16:53:05 +0900

    [Passed] 퀵 정렬 구현 (32ms, 1MB)

---
+++ trail3/퀵 정렬 구현/implement-quick-sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100000];
+
+int Partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; ++j) {
+        if (arr[j] < pivot) {
+            i++;
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+    int tmp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = tmp;
+
+    return i + 1;
+}
+
+void QuickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pos = Partition(arr, low, high);
+        QuickSort(arr, low, pos - 1);
+        QuickSort(arr, pos + 1, high);
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    QuickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    return 0;
+}
