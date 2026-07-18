b2c55501b27eb9a57500a7f79014d70d423dc97c
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-07-18 19:01:23 +0900

    [Passed] 정수 명령 처리 2 (23ms, 1MB)

---
+++ trail3/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int N;
+string command[10000];
+int A[10000];
+
+int main() {
+    cin >> N;
+
+    queue<int> q;
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> A[i];
+            q.push(A[i]);
+        }
+        if (command[i] == "pop") {
+            cout << q.front() << "\n";
+            q.pop();
+        }
+        if (command[i] == "front") {
+            cout << q.front() << "\n";
+        }
+        if (command[i] == "size") {
+            cout << q.size() << "\n";
+        }
+        if (command[i] == "empty") {
+            cout << q.empty() << "\n";
+        }
+    }
+
+    return 0;
+}
